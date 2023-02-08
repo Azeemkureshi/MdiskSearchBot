bc98e09ab950ec16c2806527e89adc3a5be0472c
Azeemkureshi <124749267+Azeemkureshi@users.noreply.github.com>
2023-02-08 07:30:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/M0vie_Request/MdiskSearchBot.git /MdiskSearchBot
+git clone https://github.com/Azeemkureshi/MdiskSearchBot.git /MdiskSearchBot
 cd /MdiskSearchBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
